89885c69533e46e38cad1b812912536149e1b512
Alan <lanzn16@outlook.com>
2020-04-02 23:12:15 -0300

    update setup eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es6',
+        singleQuote: false,
+        printWidth: 100,
+      },
+    ],
+  },
+  plugins: ['prettier'],
 };
