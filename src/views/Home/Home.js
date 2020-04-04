d1edb5bd43a1ecff232e3d8fdb7a6ed72993a7b3
Alan <lanzn16@outlook.com>
2020-04-04 17:18:30 -0300

    udpate list and image size

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -30,13 +30,13 @@ export default function Home() {
       title: "Nike",
       key: "0",
       products: [
-        {name: "Nike", key: "0", price: 345, image: MaxImage1},
-        {name: "Nike", key: "1", price: 565, image: MaxImage2},
-        {name: "Nike", key: "2", price: 860, image: MaxImage3},
-        {name: "Nike", key: "3", price: 370, image: MaxImage4},
-        {name: "Nike", key: "4", price: 299, image: MaxImage5},
-        {name: "Nike", key: "5", price: 907, image: MaxImage6},
-        {name: "Nike", key: "6", price: 1000, image: MaxImage7},
+        {name: "Nike Air Max St1", key: "0", price: 345, image: MaxImage1},
+        {name: "Nike Air Max St2", key: "1", price: 565, image: MaxImage2},
+        {name: "Nike Air Max St3", key: "2", price: 860, image: MaxImage3},
+        {name: "Nike Air Max St4", key: "3", price: 370, image: MaxImage4},
+        {name: "Nike Air Max St5", key: "4", price: 299, image: MaxImage5},
+        {name: "Nike Air Max St6", key: "5", price: 907, image: MaxImage6},
+        {name: "Nike Air Max St7", key: "6", price: 1000, image: MaxImage7},
       ],
     },
     {
@@ -116,7 +116,7 @@ export default function Home() {
               <View style={{alignItems: "flex-end", bottom: 30}}>
                 <Image
                   source={MaxImage4}
-                  style={{resizeMode: "cover", width: 230, height: 150}}
+                  style={{resizeMode: "cover", width: 200, height: 135}}
                 />
               </View>
             </LinearGradient>
