8f87e6155362a6b5405890b3d9f2b86aec73bd54
Alan <lanzn16@outlook.com>
2020-04-05 17:41:57 -0300

    remove action to press favorite

--- src/views/Favorite/ListFavorite.js
+++ src/views/Favorite/ListFavorite.js
@@ -14,16 +14,56 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import CardGradientList from "../../components/CardsGradientList";
 
 export default function Favorite({navigation}) {
-  const [favorited, setFavorited] = useState(false);
-
   const products = [
-    {name: "Nike Air Max St1", key: "0", price: 345, image: MaxImage1},
-    {name: "Nike Air Max St2", key: "1", price: 565, image: MaxImage2},
-    {name: "Nike Air Max St3", key: "2", price: 860, image: MaxImage3},
-    {name: "Nike Air Max St4", key: "3", price: 370, image: MaxImage4},
-    {name: "Nike Air Max St5", key: "4", price: 299, image: MaxImage5},
-    {name: "Nike Air Max St6", key: "5", price: 907, image: MaxImage6},
-    {name: "Nike Air Max St7", key: "6", price: 1000, image: MaxImage7},
+    {
+      name: "Nike Air Max St1",
+      key: "0",
+      price: 345,
+      image: MaxImage1,
+      favorited: true,
+    },
+    {
+      name: "Nike Air Max St2",
+      key: "1",
+      price: 565,
+      image: MaxImage2,
+      favorited: true,
+    },
+    {
+      name: "Nike Air Max St3",
+      key: "2",
+      price: 860,
+      image: MaxImage3,
+      favorited: true,
+    },
+    {
+      name: "Nike Air Max St4",
+      key: "3",
+      price: 370,
+      image: MaxImage4,
+      favorited: true,
+    },
+    {
+      name: "Nike Air Max St5",
+      key: "4",
+      price: 299,
+      image: MaxImage5,
+      favorited: true,
+    },
+    {
+      name: "Nike Air Max St6",
+      key: "5",
+      price: 907,
+      image: MaxImage6,
+      favorited: true,
+    },
+    {
+      name: "Nike Air Max St7",
+      key: "6",
+      price: 1000,
+      image: MaxImage7,
+      favorited: true,
+    },
   ];
 
   return (
@@ -35,24 +75,8 @@ export default function Favorite({navigation}) {
           outherName="Men's Shoes"
           fractionScreenDimension={3}
           ItemOnRight={() => (
-            <TouchableWithoutFeedback onPress={() => setFavorited(!favorited)}>
-              <Icon
-                name={favorited ? "heart" : "heart-outline"}
-                size={22}
-                color={favorited ? "red" : "white"}
-                style={[
-                  {
-                    alignSelf: "center",
-                  },
-                  favorited
-                    ? {
-                        textShadowColor: "rgba(0, 0, 0, 1)",
-                        textShadowOffset: {width: 1, height: 1},
-                        textShadowRadius: 10,
-                      }
-                    : {},
-                ]}
-              />
+            <TouchableWithoutFeedback onPress={() => {}}>
+              <Icon name="heart-outline" size={22} color="white" />
             </TouchableWithoutFeedback>
           )}
         />
